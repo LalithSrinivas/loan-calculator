460b6d2b2171cb56ff8855dac02a983fdf08e892
LalithSrinivas <srinivaskaladi3096@gmail.com>
2025-04-18 18:54:05 +0530

    github web page

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  output: 'export',
+  images: {
+    unoptimized: true
+  },
+  basePath: "/loan-calculator",
+  assetPrefix: "/loan-calculator/"
 };
 
-module.exports = nextConfig; 
+module.exports = nextConfig;
